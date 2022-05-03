8165a2c9db294ce14ac061a1f051eb7b8da4635a
mazharul95 <mipiyas07@gmail.com>
2022-05-04 01:34:22 +0600

    Episode 8 - Make Responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Voting') }}</title>
+        <title>Laracasts Voting</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+sans:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -17,39 +17,40 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans bg-gray-background text-gray-900 text-sm">
-        <header class="flex items-center justify-between px-8 py-4">
+        <header class="flex flex-col md:flex-row items-center justify-between px-8 py-4">
             <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
-            <div class="flex items-center">
-            @if (Route::has('login'))
-                <div class="px-6 py-4">
-                    @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a :href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }} 
-                            </a>
-                        </form> 
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+            <div class="flex items-center mt-2 md:mt-0">
+                @if (Route::has('login'))
+                    <div class="px-6 py-4">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-                <a href="">
-                <img src="{{ asset('img/img_avatar1.jpg') }}" alt="avatar" class="w-10 h-10 rounded-full">
+                                <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log out') }}
+                                </a>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+                <a href="#">
+                    <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
                 </a>
             </div>
         </header>
 
-        <main class="container mx-auto max-w-custom flex">
-            <div class="w-70 mr-5">
+        <main class="container mx-auto max-w-custom flex flex-col md:flex-row">
+            <div class="w-70 mx-auto md:mx-0 md:mr-5">
                 <div
-                    class="bg-white border-2 border-blue rounded-xl mt-16"
+                    class="bg-white md:sticky md:top-8 border-2 border-blue rounded-xl mt-16"
                     style="
                           border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
                             border-image-slice: 1;
@@ -98,8 +99,8 @@
                     </form>
                 </div>
             </div>
-            <div class="w-175">
-                <nav class="flex items-center justify-between text-xs">
+            <div class="w-full px-2 md:px-0 md:w-175">
+                <nav class="hidden md:flex items-center justify-between text-xs">
                     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                         <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
                         <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a></li>
@@ -111,6 +112,7 @@
                         <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a></li>
                     </ul>
                 </nav>
+
                 <div class="mt-8">
                     {{ $slot }}
                 </div>
@@ -118,4 +120,3 @@
         </main>
     </body>
 </html>
-
