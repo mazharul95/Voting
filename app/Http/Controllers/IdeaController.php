5e906ef6499e43620736d4118a197d334fc820bd
mazharul95 <mipiyas07@gmail.com>
2022-05-05 02:56:55 +0600

    Episode 9 - Idea Model and Migration

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use App\Http\Requests\StoreIdeaRequest;
+use App\Http\Requests\UpdateIdeaRequest;
+
+class IdeaController extends Controller
+{
+    public function index()
+    {
+        return view('idea.index',[
+            'ideas' => Idea::simplePaginate(10),
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreIdeaRequest $request)
+    {
+        //
+    }
+
+    public function show(Idea $idea)
+    {
+        return view('idea.show',[
+            'idea' => $idea,
+        ]);
+    }
+
+    public function edit(Idea $idea)
+    {
+        //
+    }
+
+    public function update(UpdateIdeaRequest $request, Idea $idea)
+    {
+        //
+    }
+
+    public function destroy(Idea $idea)
+    {
+        //
+    }
+}
